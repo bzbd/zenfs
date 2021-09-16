3aa340e1c04b6160ad384d55e5dd07c5e2eb217c
Weile Wei <weile.wei@bytedance.com>
2021-09-16 10:12:47 -0500

    add assertion in unit test instead of cout

--- test/backgroundWorker_test.cc
+++ test/backgroundWorker_test.cc
@@ -7,8 +7,10 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+#include <atomic>
 #include <chrono>
 #include <iostream>
+#include <numeric>
 #include <stdlib.h>
 #include <string>
 #include <thread>
@@ -19,44 +21,50 @@ using GFLAGS_NAMESPACE::SetUsageMessage;
 
 namespace ROCKSDB_NAMESPACE {
 
-std::string question = "Question of Life, the Universe, and Everything";
+const int num_jobs = 100000;
+std::vector<int> zone_numbers(num_jobs);
+std::atomic<int> actual_zone_number_sum(0);
 
-int deep_thought(void* arg) {
-  int elapsed_time = rand() % 1000;
-  std::this_thread::sleep_for(std::chrono::microseconds(elapsed_time));
-  return 42;
+void reset_zone_numbers_vec_and_sum() {
+  actual_zone_number_sum = 0;
+
+  for (int i = 0; i < zone_numbers.size(); i++) {
+    zone_numbers[i] = i;
+  }
 }
 
 // Assume this arg contains zone number that you wanna operate with, which is a int
-int arg_deep_thought(void* arg) {
+int sum_zone_number(void* arg) {
   int elapsed_time = rand() % 1000;
   std::this_thread::sleep_for(std::chrono::microseconds(elapsed_time));
   // Get the correct type manually
   int zone_number = *(int*) arg;
   // Do the job with the arg brings in.
-  std::cout << "operating zone:" << zone_number << std::endl;
+  actual_zone_number_sum += zone_number;
+
   // return value shows is this operation successfully done or not,
   // most commonly, 0 stands for success, other stands for failed,
   // For jobs that could failed, using the errorhandlingbgjob which 
   // will get this return value and process error handling.
   return 0;
 }
-  
-std:vector<int> zone_numbers;
-  
-int test() {
-  BackgroundWorker bg_worker;
-  int num_jobs = 1000000;
-  for (int i = 0; i < num_jobs; i++) {
-    // Assume job for each zone.
-    zone_numbers.emplace_back(i);
-    // Submit the job with zone_number[i] where hold the zone number.
-    bg_worker.SubmitJob(&ROCKSDB_NAMESPACE::arg_deep_thought, &zone_numbers[i]);
+
+int test_sum_in_background_worker() {
+  reset_zone_numbers_vec_and_sum();
+
+  {
+    BackgroundWorker bg_worker;
+    for (int i = 0; i < zone_numbers.size(); i++) {
+      // Submit the job with zone_number[i] where hold the zone number.
+      bg_worker.SubmitJob(&ROCKSDB_NAMESPACE::sum_zone_number, &zone_numbers[i]);
+    }
   }
-  
-//   for (int i = 0; i < num_jobs; i++) {
-//     bg_worker.SubmitJob(&ROCKSDB_NAMESPACE::deep_thought, &question);
-//   }
+
+  const int expected_zone_number_sum =
+      std::accumulate(zone_numbers.cbegin(), zone_numbers.cend(), 0);
+
+  assert(actual_zone_number_sum == expected_zone_number_sum);
+
   return 0;
 }
 }  // namespace ROCKSDB_NAMESPACE
@@ -68,5 +76,5 @@ int main(int argc, char **argv) {
 
   gflags::ParseCommandLineFlags(&argc, &argv, true);
 
-  return ROCKSDB_NAMESPACE::test();
+  return ROCKSDB_NAMESPACE::test_sum_in_background_worker();
 }
