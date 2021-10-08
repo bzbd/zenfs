bdc8627589a60dd8bc189c0279891c592503dbbf
Changlong Chen <levisonchen@live.cn>
2021-10-08 10:51:08 +0800

    Change include file path.
    * Include path has been changed, adapt that.

--- test/backgroundWorker_test.cc
+++ test/backgroundWorker_test.cc
@@ -2,8 +2,8 @@
 #include <fcntl.h>
 #include <gflags/gflags.h>
 #include <rocksdb/file_system.h>
-#include <third-party/zenfs/fs/fs_zenfs.h>
-#include <third-party/zenfs/fs/zbd_zenfs.h>
+#include <fs/fs_zenfs.h>
+#include <fs/zbd_zenfs.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
